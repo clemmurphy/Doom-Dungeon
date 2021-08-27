305fc7f28219d3d672f36bd442fe07a53df02834
Clement Murphy <clemmurphy@gmail.com>
2021-08-27 17:28:39 +0100

    Updated JS file and added lots of assets

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,67 @@
 
 function init() {
 
+  const game = {
+    gameRunning: false,
+    score: 0,
+    highScore: 0,
+    audio: document.getElementById('player')
+  }
+
+  const player = {
+    location: 0,
+    super: false,
+    animTime: 500
+  } 
+
+  const gridWidth = 28
+  const gridHeight = 36
+  const cellCount = gridWidth * gridHeight
+  const cellMap = []
+  const stageOne = [
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,5,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,5,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,6,1,1,6,1,1,1,1,1,0,1,1,1,1,1,1,
+    6,6,6,6,6,1,0,1,1,1,1,1,6,1,1,6,1,1,1,1,1,0,1,6,6,6,6,6,
+    6,6,6,6,6,1,0,1,6,6,6,6,6,6,6,6,6,6,6,6,1,0,1,6,6,6,6,6,
+    6,6,6,6,6,1,0,1,6,1,1,1,1,2,2,1,1,1,1,6,1,0,1,6,6,6,6,6,
+    1,1,1,1,1,1,0,1,6,1,2,2,2,2,2,2,2,2,1,6,1,0,1,1,1,1,1,1,
+    4,6,6,6,6,6,0,6,6,1,2,2,2,2,2,2,2,2,1,6,6,0,6,6,6,6,6,4,
+    1,1,1,1,1,1,0,1,6,1,2,2,2,2,2,2,2,2,1,6,1,0,1,1,1,1,1,1,
+    6,6,6,6,6,1,0,1,6,1,1,1,1,1,1,1,1,1,1,6,1,0,1,6,6,6,6,6,
+    6,6,6,6,6,1,0,1,6,6,6,6,6,6,6,6,6,6,6,6,1,0,1,6,6,6,6,6,
+    6,6,6,6,6,1,0,1,6,1,1,1,1,1,1,1,1,1,1,6,1,0,1,6,6,6,6,6,
+    1,1,1,1,1,1,0,1,6,1,1,1,1,1,1,1,1,1,1,6,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,5,0,0,1,1,0,0,0,0,0,0,0,6,3,0,0,0,0,0,0,0,1,1,0,0,5,1,
+    1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+    6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+  ]
+
+  buildArena(stageOne)
+  document.addEventListener('keyup', handleKeyPress)
+
   // ! FUNCTIONS
   
   // menuload
@@ -24,48 +85,6 @@ function init() {
     // remove player
     // remove arena
     // run menuload
-  
-  const gridWidth = 28
-  const gridHeight = 36
-  const cellCount = gridWidth * gridHeight
-  const cellMap = []
-  const stageOne = [
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
-    0,0,0,0,0,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,
-    0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,
-    0,0,0,0,0,1,0,1,0,1,1,1,1,2,2,1,1,1,1,0,1,0,1,0,0,0,0,0,
-    1,1,1,1,1,1,0,1,0,1,2,2,2,2,2,2,2,2,1,0,1,0,1,1,1,1,1,1,
-    4,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,4,
-    1,1,1,1,1,1,0,1,0,1,2,2,2,2,2,2,2,2,1,0,1,0,1,1,1,1,1,1,
-    0,0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0,0,
-    0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,
-    0,0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0,0,
-    1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
-    1,0,0,0,1,1,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1,1,0,0,0,1,
-    1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,
-    1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,
-    1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-  ]
 
   function buildArena(stage) {
     console.log('Building arena')
@@ -74,51 +93,127 @@ function init() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.classList.add('cell')
-      cell.innerText = i
+      // cell.innerText = i
       // check current stage map
-      if (stage[i] === 1) {
+      if (stage[i] === 0) {
+        // add pellets
+        cell.classList.add('pellet')
+      } else if (stage[i] === 1) {
         // make edges of grid into walls
         cell.classList.add('wall')
-      } else if (stage[i] === 3) {
-        // place player
-        cell.classList.add('player')
       } else if (stage[i] === 2) {
         // set up ghost house
         cell.classList.add('ghost-house')
+      } else if (stage[i] === 3) {
+        // place player
+        cell.classList.add('player')
+        player.location = i
       } else if (stage[i] === 4) {
         // set up teleport zones
         cell.classList.add('teleport-zone')
+      } else if (stage[i] === 5) {
+        // add powerups
+        cell.classList.add('powerup')
       }
       cellMap.push(cell)
       grid.appendChild(cell)
     }
   }
 
-  buildArena(stageOne)
+  function removePlayer(loc) {
+    cellMap[loc].classList.remove('player')
+  }
 
-  // buildArena
-    
-    // place walls and empty squares
-    // place powerups
-    // place pellets
-    // set player start location
-    // place player
-    // place ghosts
+  function addPlayer(loc) {
+    const targetCell = cellMap[loc].classList
+    if (targetCell.contains('pellet')) {
+      game.score += 10
+      game.audio.src = 'assets/sounds/pellet.mp3'
+      game.audio.play()
+      targetCell.remove('pellet')
+    } else if (targetCell.contains('powerup')) {
+      game.score += 50
+      player.super = true
+      game.audio.src = 'assets/sounds/powerup.mp3'
+      game.audio.play()
+      targetCell.remove('powerup')
+    }
+    cellMap[loc].classList.add('player')
+  }
 
-  // update
-    // // every 10ms*
-    // on move
-      // check for collisions
-        // if collision between player in normal state and ghost
-          // player dies
-        // if collision between player in super state and ghost
-          // ghost dies
-        // if collision between player and pellet
-          // increment score
-          // rid of pellet
-        // if collision between player and powerup
-          // player enters super state
-          // ghosts enter afraid state
+  function handleKeyPress(event) {
+
+    const prev = player.location
+    const key = event.key
+
+    // WASD controls
+    const w = 'w'
+    const a = 'a'
+    const s = 's'
+    const d = 'd'
+    // arrow key controls
+    const upArrow = 'ArrowUp'
+    const downArrow = 'ArrowDown'
+    const leftArrow = 'ArrowLeft'
+    const rightArrow = 'ArrowRight'
+
+    if (key === w || key === upArrow) {
+      console.log('Pressed up')
+      const aboveProps = cellMap[player.location - gridWidth].classList
+
+      // check to make sure the intended destination should be accessible
+      if (!aboveProps.contains('wall')) {
+        player.location -= gridWidth
+        // // animate the movement
+        // cellMap[prev].classList.add('move-up')
+        // setTimeout(cellMap[prev].classList.remove('move-up'), player.animTime)
+      }
+    } else if (key === a || key === leftArrow) {
+      console.log('Pressed left')
+      const leftProps = cellMap[player.location - 1].classList
+
+      // check to make sure the intended destination should be accessible
+      if (!leftProps.contains('wall')) {
+        
+        // cellMap[player.location].classList.add('move-left')
+        player.location--
+        // // animate the movement
+        // setTimeout(() => {
+        //   cellMap[prev].classList.remove('move-left')
+        // }, player.animTime)
+      }
+    } else if (key === s || key === downArrow) {
+      console.log('Pressed down')
+      const belowProps = cellMap[player.location + gridWidth].classList
+
+      // check to make sure the intended destination should be accessible
+      if (!belowProps.contains('wall')) {
+        player.location += gridWidth
+        // // animate the movement
+        // cellMap[prev].classList.add('move-down')
+        // setTimeout(cellMap[prev].classList.remove('move-down'), player.animTime)
+      }
+    } else if (key === d || key === rightArrow) {
+      console.log('Pressed right')
+      const rightProps = cellMap[player.location + 1].classList
+
+      // check to make sure the intended destination should be accessible
+      if (!rightProps.contains('wall')) {
+        player.location++
+        // // animate the movement
+        // cellMap[prev].classList.add('move-right')
+        // setTimeout(() => {
+        //   cellMap[prev].classList.remove('move-right')
+        // }, player.animTime)
+      }
+    } else {
+      console.log(`Key "${event.key}" not recognised!`)
+    }
+    removePlayer(prev)
+    addPlayer(player.location)
+  }
+
+
       // check number of pellets left
         // if 10 gone, spawn first ghost
         // if 30 gone, spawn second ghost
@@ -167,34 +262,18 @@ function init() {
 
   // ! OBJECTS
 
-  // game manager
-    // bool game started
-    // current score
-    // high score
+  class Ghost {
+    constructor(position, target, scared) {
+      this.position = position,
+      this.target = target,
+      this.scared = scared
+    }
+  }
 
-  // player manager
-    // player current location
-    // bool super state
-    // input listener
-      // if not trying to move outside of arena
-      // and not trying to move into a wall
-      // move player
-
-  // ghost manager
-    // blinky current position
-    // blinky target position - player position
-    // bool blinky active state
-    // bool scared state
-
-    // pinky current position
-    // pinky target position
-    // bool pinky active state
-    // inky current position
-    // inky target position
-    // bool inky active state
-    // clyde current position
-    // clyde target position
-    // bool clyde active state
+  const blinky = new Ghost(0, 0, false)
+  const pinky = new Ghost(0, 0, false)
+  const inky = new Ghost(0, 0, false)
+  const clyde = new Ghost(0, 0, false)
 }
 
 window.addEventListener('DOMContentLoaded', init)
